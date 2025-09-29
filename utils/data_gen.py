556fc9e26fad894482478d9fe6c465dcbe045702
Taras Voronovich <taras.varanovich@gmail.com>
2025-09-29 20:53:21 +0200

    marked necessary note

--- utils/data_gen.py
+++ utils/data_gen.py
@@ -17,7 +17,7 @@ def generate_row(date_low_b, date_up_b, val_low_b, val_up_b):
 def main():
     start_date = datetime.datetime(2022, 1, 1, 0, 0, 0)
     end_date = datetime.datetime(2023, 1, 1, 0, 0, 0)
-    filename = f"workouts_raw_{int(time.time())}.txt"
+    filename = f"workouts_one_user_raw_{int(time.time())}.txt"
     with open(filename, "wt", 1) as source_file:
         for _ in range(50):
             value, date = generate_row(start_date, end_date, 0.0, 100.0)
