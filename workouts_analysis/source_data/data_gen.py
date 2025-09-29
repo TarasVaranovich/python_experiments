12e19512b1aa65e808430f6119477e1b8c2e1122
Taras Voronovich <taras.varanovich@gmail.com>
2025-09-29 21:01:31 +0200

    source data generator moved to appropriate package

--- workouts_analysis/source_data/data_gen.py
+++ workouts_analysis/source_data/data_gen.py
@@ -4,7 +4,6 @@ import time
 
 
 def generate_row(date_low_b, date_up_b, val_low_b, val_up_b):
-
     start_ts = int(date_low_b.timestamp())
     end_ts = int(date_up_b.timestamp())
 
@@ -17,12 +16,13 @@ def generate_row(date_low_b, date_up_b, val_low_b, val_up_b):
 def main():
     start_date = datetime.datetime(2022, 1, 1, 0, 0, 0)
     end_date = datetime.datetime(2023, 1, 1, 0, 0, 0)
-    filename = f"workouts_one_user_raw_{int(time.time())}.txt"
+    filename = f"workouts_one_user_raw_{int(time.time())}.csv"
     with open(filename, "wt", 1) as source_file:
         for _ in range(50):
             value, date = generate_row(start_date, end_date, 0.0, 100.0)
             print(f"{date},{value:.2f}")
             source_file.write(f"{date},{value:.2f}\n")
     source_file.close()
+
 
 main()
