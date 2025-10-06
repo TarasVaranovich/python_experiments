4920c4853bec31e7917d5a7eca449354aff97d7a
Taras Voronovich <taras.varanovich@gmail.com>
2025-10-06 20:34:02 +0200

    data sorted

--- workouts_analysis/__init__.py
+++ workouts_analysis/__init__.py
@@ -1,1 +1,2 @@
 WORKOUT_STATS_PATH = "../source_data/workouts_one_user_raw_1759172457.csv"
+WORKOUT_STATS_SORTED = "../source_data/workouts_sorted_1759775585.csv"
--- workouts_analysis/transform/workout_transform.py
+++ workouts_analysis/transform/workout_transform.py
@@ -1,10 +1,14 @@
+import time
+
 import pandas as pd
 
 from workouts_analysis import WORKOUT_STATS_PATH
 
 
 def main():
-    workouts_df = pd.read_csv(WORKOUT_STATS_PATH)
-    print(workouts_df)
+    workouts_df = pd.read_csv(WORKOUT_STATS_PATH, header=None, names=["timestamp", "value"])
+    workouts_df.sort_values("timestamp", inplace=True)
+    workouts_df["timestamp"] = pd.to_datetime(workouts_df["timestamp"])
+    workouts_df.to_csv(f"../source_data/workouts_sorted_{int(time.time())}.csv",index=False)
 
 main()
