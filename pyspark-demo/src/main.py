0797256f00a2fc3a8f200ff58807c7fca249690b
Taras Voronovich <tarasvoronovich@coherentsolutions.com>
2025-10-20 14:40:09 +0200

    added row size calculation in spark

--- pyspark-demo/src/main.py
+++ pyspark-demo/src/main.py
@@ -1,6 +1,13 @@
 from pyspark.sql import SparkSession
 import os
 
+from pyspark.sql.functions import col, udf
+from pyspark.sql.types import size, LongType
+
+
+def row_size(*cols):
+  return sum([len(str(c).encode('utf-8')) for c in cols])
+
 def main():
     spark = (
         SparkSession.builder
@@ -12,8 +19,9 @@ def main():
     print(f"JAVA_HOME: {os.getenv('JAVA_HOME')}")
 
     df = spark.read.csv("../data/products-100000.csv", header=True, inferSchema=True)
-    df.show(5)
-    # Investigate files archiving into avro, parquet, orc etc..
+    row_size_udf = udf(row_size, LongType())
+    df_with_size = df.withColumn("row_size", row_size_udf(*df.columns))
+    df_with_size.show(10)
     spark.stop()
 
 if __name__ == "__main__":
